6ec7051a7ed069eee5f51b63a63e997d3c946f46
AlejandroDalzotto <aledalzotto15@gmail.com>
2022-10-22 15:02:40 -0300

    Se agrega el menu

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -10,6 +10,19 @@ export class GameComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.navbar();
+  }
+
+  navbar(): void {
+    const btn = (document.querySelector('.btn') as HTMLButtonElement);
+    const nav = (document.querySelector('.navbar') as HTMLElement);
+
+    
+    btn.addEventListener('click', (): void => {
+      nav.classList.toggle('activo');
+      btn.classList.toggle('btn-white');
+    });
   }
 
 }
+
