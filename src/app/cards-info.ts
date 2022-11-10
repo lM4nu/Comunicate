b8622403d7ae6b937da8bc13143bd5dccaa814a7
M4nu <manuolakeasesosa@gmail.com>
2022-11-10 15:46:27 -0300

    fix

--- src/app/cards-info.ts
+++ src/app/cards-info.ts
@@ -2,8 +2,8 @@ export const DATA: any[] = [
   {
     id: 1,
     title: 'Bosque',
-    img: 'assets/img/wallpaper_10.jpg',
+    img: '/Comunicate/assets/img/wallpaper_10.jpg',
     delete: false,
   },
-  { id: 2, title: 'Gato', img: 'assets/img/wallpaper_21.jpg', delete: false },
+  { id: 2, title: 'Gato', img: '/Comunicate/assets/img/wallpaper_21.jpg', delete: false },
 ];
