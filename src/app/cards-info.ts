416e71cd5be974ee8b208fda7cfd6e865841b0c7
M4nu <manuolakeasesosa@gmail.com>
2022-11-17 13:16:59 -0300

    reduce size

--- src/app/cards-info.ts
+++ src/app/cards-info.ts
@@ -2,8 +2,8 @@ export const DATA: any[] = [
   {
     id: 1,
     title: 'Bosque',
-    img: '/Comunicate/assets/img/wallpaper_10.jpg',
+    img: '/Comunicate/assets/img/wallpaper_10.webp',
     delete: false,
   },
-  { id: 2, title: 'Gato', img: '/Comunicate/assets/img/wallpaper_21.jpg', delete: false },
+  { id: 2, title: 'Gato', img: '/Comunicate/assets/img/wallpaper_21.webp', delete: false },
 ];
